35d988c08692db7dbb3dc596dd90ae584a398bb1
Bob Hunt <bob.hunt.email@gmail.com>
2021-04-21 17:10:29 -0400

    Fixed typo. Eliminated error by adding a key to courseGoals.map

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
-  const [currentGoals, setCurrentGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([]);
 
 // function goalInputHandler(enteredText) {
   // Changed the function above to and arrow function below.
@@ -11,8 +11,8 @@ const goalInputHandler = (enteredText) => {
   setEnteredGoal(enteredText);
 };
 
-const addGoalHandler = (enteredText) => {
-  setCourseGoals([currentGoals => [...currentGoals, enteredGoal]);
+const addGoalHandler = () => {
+  setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
 };
 
   return (
@@ -27,9 +27,7 @@ const addGoalHandler = (enteredText) => {
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-        <Text>
-
-        </Text>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
